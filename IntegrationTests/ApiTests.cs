7d4674b091601d7861643553ed3ce5c7a7d10773
George Church <15802979+gchurch@users.noreply.github.com>
2021-08-04 14:55:40 +0100

    Created a couple more integration tests for the teachers API.

--- IntegrationTests/ApiTests.cs
+++ IntegrationTests/ApiTests.cs
@@ -43,7 +43,7 @@ namespace IntegrationTests
         }
 
         [TestMethod]
-        public async Task GetRequestOfTeachersEndpoint_ShouldReturnExpectedTeacherData()
+        public async Task GetRequestOfTeachersEndpoint_ShouldReturnExpectedTeachersData()
         {
             // Arrange
             string url = "/api/teachers/";
@@ -58,6 +58,48 @@ namespace IntegrationTests
             Assert.AreEqual("David", teachers[0].FirstName);
             Assert.AreEqual("Michelle", teachers[1].FirstName);
             Assert.AreEqual("John", teachers[2].FirstName);
+        }
+
+        [TestMethod]
+        public async Task GetRequestOfTeachersEndpointWithId_ShouldReturnExpectedTeacherData()
+        {
+            // Arrange
+            int id = 1;
+            string url = "/api/teachers/" + id;
+            HttpClient client = _factory.CreateClient();
+
+            // Act
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            // Assert
+            Teacher teacher = await DeserializeResponse<Teacher>(response);
+            Assert.AreEqual("David", teacher.FirstName);
+            Assert.AreEqual("Allen", teacher.LastName);
+            Assert.AreEqual(30, teacher.Age);
+            Assert.AreEqual("Maths", teacher.Subject);
+        }
+
+        [TestMethod]
+        public async Task PostRequestOfTeachersToTeachersEndpoint_ShouldAddTheTeacher()
+        {
+            // Arrange
+            HttpClient client = _factory.CreateClient();
+
+            var teacherToPost = new Teacher()
+            {
+                FirstName = "Peter"
+            };
+            string url = "/api/teachers/";
+            StringContent serializedTeacher = SerializeObject(teacherToPost);
+
+            // Act
+            HttpResponseMessage response = await client.PostAsync(url, serializedTeacher);
+
+            // Assert
+            Teacher teacherResponse = await DeserializeResponse<Teacher>(response);
+            Assert.AreEqual(teacherToPost.FirstName, teacherResponse.FirstName);
+            int expectedIdOfNewTeacher = 4;
+            Assert.AreEqual(expectedIdOfNewTeacher, teacherResponse.TeacherId);
         }
     }
 }
